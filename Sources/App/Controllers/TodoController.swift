10f7e8bcc4a95ed8a39482705bbae16ec30bb27d
Valentin Knabel <dev@vknabel.com>
2018-11-15 20:21:56 +0100

    Code format

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -1,5 +1,5 @@
-import Vapor
 import FluentSQLite
+import Vapor
 
 /// Simple todo-list controller.
 final class TodoController {
@@ -7,7 +7,7 @@ final class TodoController {
     func index(_ req: Request) throws -> Future<[Todo]> {
         // fetch auth'd user
         let user = try req.requireAuthenticated(User.self)
-        
+
         // query all todo's belonging to user
         return try Todo.query(on: req)
             .filter(\.userID == user.requireID()).all()
@@ -17,11 +17,11 @@ final class TodoController {
     func create(_ req: Request) throws -> Future<Todo> {
         // fetch auth'd user
         let user = try req.requireAuthenticated(User.self)
-        
+
         // decode request content
         return try req.content.decode(CreateTodoRequest.self).flatMap { todo in
             // save new todo
-            return try Todo(title: todo.title, userID: user.requireID())
+            try Todo(title: todo.title, userID: user.requireID())
                 .save(on: req)
         }
     }
@@ -30,14 +30,14 @@ final class TodoController {
     func delete(_ req: Request) throws -> Future<HTTPStatus> {
         // fetch auth'd user
         let user = try req.requireAuthenticated(User.self)
-        
+
         // decode request parameter (todos/:id)
         return try req.parameters.next(Todo.self).flatMap { todo -> Future<Void> in
             // ensure the todo being deleted belongs to this user
             guard try todo.userID == user.requireID() else {
                 throw Abort(.forbidden)
             }
-            
+
             // delete model
             return todo.delete(on: req)
         }.transform(to: .ok)
